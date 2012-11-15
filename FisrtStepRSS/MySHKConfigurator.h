9fec5cdbde4ee28e22022b72169352d6064f3d22
Sealed <User@Lions-Mac.local>
2012-11-15 18:19:19 +0200

    withsubmodules

---
+++ FisrtStepRSS/MySHKConfigurator.h
@@ -0,0 +1,13 @@
+//
+//  MySHKConfigurator.h
+//  FisrtStepRSS
+//
+//  Created by Admin on 13/11/2012.
+//  Copyright (c) 2012 Vadim Glushko. All rights reserved.
+//
+
+#import "DefaultSHKConfigurator.h"
+
+@interface MySHKConfigurator : DefaultSHKConfigurator
+
+@end
---
+++ FisrtStepRSS/MySHKConfigurator.m
@@ -0,0 +1,13 @@
+//
+//  MySHKConfigurator.m
+//  FisrtStepRSS
+//
+//  Created by Admin on 13/11/2012.
+//  Copyright (c) 2012 Vadim Glushko. All rights reserved.
+//
+
+#import "MySHKConfigurator.h"
+
+@implementation MySHKConfigurator
+
+@end
